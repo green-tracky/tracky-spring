482f9f92facfa344f8190594fff611c9df2848e3
roni243 <ksri2457@gmail.com>
2025-06-24 15:31:06 +0900

    [Feat] 러닝 뱃지 조회 (#5)
    * 환경설정 중간저장
    
    * 환경설정 중간저장
    
    * 패키지 구조 & 에러처리 & 응답객체 & 프로퍼티 세팅완료
    
    * 어플리케이션 프로퍼티 로컬 파일 주석 스페이스바 1칸 멀어진거 수정
    
    * 수정
    
    * 수정
    
    * dev-properties 파일 공백 수정
    
    * s 붙은 폴더 삭제 안된거 수정
    
    * error 패키지 .keep 파일 삭제
    
    * 프로퍼티 주석 위치 수정
    
    * .
    
    * 칼로리계산 & 페이스계산 유틸함수
    
    * dto 만드는 중
    
    * 중간저장
    
    * 엔티티 시간필드 수정
    
    * 날짜 수정 후 엔티티의 생성자 수정
    
    * 중간저장
    
    * 엔티티 필드값 수정 & 이넘 추가
    
    * 요청 DTO 완성
    
    * 요청 DTO toEntity 완성
    
    * save 테스트 완
    
    * run 관련 엔티티 OneToMany 필드값 빌더 생성자에서 제거
    
    * RunRecord 엔티티 totalDistanceMeters 의 타입 Double -> Integer 변경
    
    * 중간저장
    
    * RunRecord avgPace 변수값 avg_pace 로 되어있던거 원래대로 돌림
    
    * 중간저장
    
    * 사진 데이터 수정. 위도 경도 따로 추가 & 사진의 생성날짜 어노테이션 제거
    
    * 전 커밋 메시지 동일
    
    * 중간저장
    
    * Picture 생성자 수정
    
    * save 에 대한 응답 DTO 만듦
    
    * 통합테스트 만들기전
    
    * 중간저장
    
    * 통합테스트 세팅
    
    * 러닝등록 완료(사진빼고)
    
    * s 붙은 폴더 삭제
    
    * s 붙은 폴더 삭제
    
    * 엔티티 수정 RunRecord & RunSegment. 요청 DTO 수정
    
    * 중간저장
    
    * 응답 DTO 수정
    
    * 더미만듦
    
    * 더미만듦
    
    * 더미만듦
    
    * 더미만듦
    
    * 더미만듦
    
    * 수정된 엔티티로 테스트
    
    * findAll 테스트 작성
    
    * findAll 테스트 작성
    
    * 러닝 등록 완 - 사진 제외
    
    * 페이스 계산 함수 수정
    
    * findAll 테스트 수정
    
    * 페이스 계산 수정
    
    * RunBadge 수정
    
    * 메서드 이름 수정
    
    * RunRecordResponse에 DTO 에 필드에 필요없는 주석 제거
    
    * run-badge 조회
    
    * 충돌 해결
    
    ---------
    
    Co-authored-by: harimmon <bhr0502@gmail.com>
    Co-authored-by: jjack-1 <qazwsx0088@gmail.com>

--- src/main/java/com/example/tracky/runrecord/runbadge/RunBadgeController.java
+++ src/main/java/com/example/tracky/runrecord/runbadge/RunBadgeController.java
@@ -1,15 +1,28 @@
 package com.example.tracky.runrecord.runbadge;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.tracky._core.utils.Resp;
 import com.example.tracky.runrecord.runbadge.runbadgeachv.RunBadgeAchvService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/s/api")
 public class RunBadgeController {
 
-    private final RunBadgeAchvService runRecordsService;
+    private final RunBadgeService runBadgeService;
+
+    @GetMapping("/run-badges")
+    public ResponseEntity<?> getRunBadges() {
+        List<RunBadgeResponse.DTO> respDTO = runBadgeService.getRunBadges();
+        return Resp.ok(respDTO);
+    }
 
 }
