547cca9b06dd02946b56bac9d52e3dc2d1e3410f
jjack-1 <qazwsx0088@gmail.com>
2025-06-25 16:59:58 +0900

    [Feat] 러닝 상세 조회 (#8)
    * 환경설정 중간저장
    
    * 환경설정 중간저장
    
    * 패키지 구조 & 에러처리 & 응답객체 & 프로퍼티 세팅완료
    
    * 어플리케이션 프로퍼티 로컬 파일 주석 스페이스바 1칸 멀어진거 수정
    
    * 수정
    
    * 수정
    
    * dev-properties 파일 공백 수정
    
    * s 붙은 폴더 삭제 안된거 수정
    
    * error 패키지 .keep 파일 삭제
    
    * 프로퍼티 주석 위치 수정
    
    * .
    
    * 칼로리계산 & 페이스계산 유틸함수
    
    * dto 만드는 중
    
    * 중간저장
    
    * 엔티티 시간필드 수정
    
    * 날짜 수정 후 엔티티의 생성자 수정
    
    * 중간저장
    
    * 엔티티 필드값 수정 & 이넘 추가
    
    * 요청 DTO 완성
    
    * 요청 DTO toEntity 완성
    
    * save 테스트 완
    
    * run 관련 엔티티 OneToMany 필드값 빌더 생성자에서 제거
    
    * RunRecord 엔티티 totalDistanceMeters 의 타입 Double -> Integer 변경
    
    * 중간저장
    
    * RunRecord avgPace 변수값 avg_pace 로 되어있던거 원래대로 돌림
    
    * 중간저장
    
    * 사진 데이터 수정. 위도 경도 따로 추가 & 사진의 생성날짜 어노테이션 제거
    
    * 전 커밋 메시지 동일
    
    * 중간저장
    
    * Picture 생성자 수정
    
    * save 에 대한 응답 DTO 만듦
    
    * 통합테스트 만들기전
    
    * 중간저장
    
    * 통합테스트 세팅
    
    * 러닝등록 완료(사진빼고)
    
    * s 붙은 폴더 삭제
    
    * s 붙은 폴더 삭제
    
    * 엔티티 수정 RunRecord & RunSegment. 요청 DTO 수정
    
    * 중간저장
    
    * 응답 DTO 수정
    
    * 수정된 엔티티로 테스트
    
    * 러닝 등록 완 - 사진 제외
    
    * 페이스 계산 함수 수정
    
    * 페이스 계산 수정
    
    * RunRecordResponse에 DTO 에 필드에 필요없는 주석 제거
    
    * 중간저장
    
    * RunRecordUtil에 있는 calculateTotalDurationSeconds 의 인수 변수이름 변경
    
    * 날짜포맷등록
    
    * 중간저장
    
    * êRunBadge Service 수정 및 주석
    
    * db 파일 분산
    
    * 단일조í ㅏ만ë들기전
    
    * 1건요청 처리 완
    
    * 러닝 등롯록시 유저 엔티티를  받아서 처리하는 방법으로 변경
    
    * data.sql 파일  삭제
    
    * 러러닝 상세조회 완
    
    * .
    
    ---------
    
    Co-authored-by: harimmon <bhr0502@gmail.com>

--- src/main/java/com/example/tracky/runrecord/RunRecordService.java
+++ src/main/java/com/example/tracky/runrecord/RunRecordService.java
@@ -1,12 +1,11 @@
 package com.example.tracky.runrecord;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.example.tracky._core.error.ErrorCodeEnum;
 import com.example.tracky._core.error.ex.ExceptionApi404;
-
+import com.example.tracky.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,23 +13,31 @@ public class RunRecordService {
 
     private final RunRecordRepository runRecordsRepository;
 
-    public void getRunRecord(Integer id) {
+    /**
+     * 러닝 상세 조회
+     *
+     * @param runRecordId
+     * @return
+     */
+    public RunRecordResponse.DetailDTO getRunRecord(Integer runRecordId) {
         // 러닝 기록 조회
-        RunRecord runRecord = runRecordsRepository.findById(id)
+        RunRecord runRecord = runRecordsRepository.findByIdJoin(runRecordId)
                 .orElseThrow(() -> new ExceptionApi404(ErrorCodeEnum.RUN_NOT_FOUND));
 
+        // 러닝 응답 DTO 로 변환
+        return new RunRecordResponse.DetailDTO(runRecord);
     }
 
     /**
      * 러닝 저장
-     * 
-     * @param userId
+     *
+     * @param user
      * @param reqDTO
      */
     @Transactional
-    public RunRecordResponse.SaveDTO save(Integer userId, RunRecordRequest.SaveDTO reqDTO) {
+    public RunRecordResponse.SaveDTO save(User user, RunRecordRequest.SaveDTO reqDTO) {
         // 엔티티 변환
-        RunRecord runRecord = reqDTO.toEntity(userId);
+        RunRecord runRecord = reqDTO.toEntity(user);
 
         // 엔티티 저장
         RunRecord runRecordPS = runRecordsRepository.save(runRecord);
