e0b0649bc63af581ef769066203ff58c2cf81ff6
roni243 <ksri2457@gmail.com>
2025-06-27 09:19:33 +0900

    챌린지 조회

--- src/main/resources/db/01-user-data.sql
+++ src/main/resources/db/01-user-data.sql
@@ -17,11 +17,3 @@ INSERT INTO user_tb (username, email, profile_url, height, weight, gender, user_
                      flutter_token_id, created_at)
 VALUES ('love', 'love@example.com', 'http://example.com/profiles/love.jpg', 160.0, 55.0, '여', '일반', 'kakao',
         '#123ABC', 'token_love_789', NOW());
-
-
-
-INSERT INTO run_level_tb (name, min_distance, max_distance, description, image_url, sort_order)
-VALUES ('옐로우', 0, 49999, '입문 러너', 'https://example.com/images/yellow.png', 1),
-       ('오렌지', 50000, 249999, '기초 러너', 'https://example.com/images/orange.png', 2),
-       ('그린', 250000, 999999, '중급 러너', 'https://example.com/images/green.png', 3),
-       ('블루', 1000000, 2499000, '상급 러너', 'https://example.com/images/blue.png', 4);
