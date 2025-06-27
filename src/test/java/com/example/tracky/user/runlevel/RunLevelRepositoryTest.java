9bb73b3976101509c95d5d3b2f94795c75a6ae0c
roni243 <ksri2457@gmail.com>
2025-06-27 15:18:12 +0900

    런레벨 수정

--- src/test/java/com/example/tracky/user/runlevel/RunLevelRepositoryTest.java
+++ src/test/java/com/example/tracky/user/runlevel/RunLevelRepositoryTest.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 @Slf4j
+@Import(RunLevelRepository.class)
 @DataJpaTest
 public class RunLevelRepositoryTest {
 
