a2a4b204fbe7d7b4da1b30db3e561fcda89ddcff
roni243 <ksri2457@gmail.com>
2025-06-30 11:43:41 +0900

    더미ì미 수정

--- src/main/resources/db/07-comment-data.sql
+++ src/main/resources/db/07-comment-data.sql
@@ -1,6 +1,6 @@
 -- 1. love가 댓글 작성
-INSERT INTO comment_tb (post_id, user_id, content, created_at, updated_at)
-VALUES (1, 3, '멋진 기록이에요!', NOW(), NOW());
+INSERT INTO comment_tb (user_id, post_id, content, created_at, updated_at)
+VALUES (3, 1, '멋진 기록이에요!', NOW(), NOW());
 -- 2. ssar가 대댓글 작성 (parent_id = 1)
-INSERT INTO comment_tb (post_id, user_id, content, parent_id, created_at, updated_at)
+INSERT INTO comment_tb (user_id, post_id, content, parent_id, created_at, updated_at)
 VALUES (1, 1, '감사합니다 :)', 1, NOW(), NOW());
