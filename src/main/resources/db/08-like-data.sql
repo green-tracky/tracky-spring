f6dd3c6ed644c9662ea2ebd8624b929a4a3e8a53
roni243 <ksri2457@gmail.com>
2025-06-30 11:37:14 +0900

    더미 수정

--- src/main/resources/db/08-like-data.sql
+++ src/main/resources/db/08-like-data.sql
@@ -1,5 +1,5 @@
--- 1. love가 1번글에 좋아요
--- 2. 게시글 작성자가 (1번) 댓글 1번을 좋아요
+-- 1. love(3번)가 1번글에 좋아요
+-- 2. 게시글 작성자(1번)가 댓글 1번에 좋아요
 INSERT INTO like_tb (post_id, user_id, comment_id, created_at)
 VALUES (1, 3, NULL, NOW()),
-       (1, 1, 1, NOW());
+       (NULL, 1, 1, NOW());
