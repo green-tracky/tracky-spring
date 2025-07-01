d2b2940f06a61d8e8408570997fba6518d9334ff
roni243 <ksri2457@gmail.com>
2025-07-01 15:36:48 +0900

    포스트 테스트코드 작성

---
+++ src/test/java/com/example/tracky/community/post/PostRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.tracky.community.post;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@Slf4j
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void findAllJoinRunRecord_test() {
+        List<Post> posts = postRepository.findAllJoinRunRecord();
+        for (Post post : posts) {
+            log.info("결과확인===================");
+            log.info("post.id: {}", post.getId());
+            log.info("post.title: {}", post.getTitle());
+
+            if (post.getUser() != null) {
+                log.info("user.id: {}", post.getUser().getId());
+                log.info("user.username: {}", post.getUser().getUsername());
+            }
+
+            if (post.getRunRecord() != null) {
+                log.info("runRecord.id: {}", post.getRunRecord().getId());
+                log.info("runRecord.memo: {}", post.getRunRecord().getMemo());
+            }
+        }
+    }
+
+}
