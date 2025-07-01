4e762e21ecc9bba41d237c90f6580f3a7b53ffeb
roni243 <ksri2457@gmail.com>
2025-07-01 10:15:39 +0900

    listDTO추가

--- src/main/java/com/example/tracky/community/post/PostResponse.java
+++ src/main/java/com/example/tracky/community/post/PostResponse.java
@@ -2,12 +2,36 @@ package com.example.tracky.community.post;
 
 import com.example.tracky.community.post.comment.CommentResponse;
 import com.example.tracky.runrecord.RunRecordResponse;
+import com.example.tracky.runrecord.picture.Picture;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class PostResponse {
+
+    @Data
+    public static class listDTO {
+        private final Integer id;
+        private final String username;
+        private final String content;
+        private final LocalDateTime createdAt;
+        private final List<Picture> pictures;
+        private final Integer loveCount;
+        private final Integer commentCount;
+        private final Boolean isLiked;
+
+        public listDTO(Post post, Integer loveCount, Integer commentCount, Boolean isLiked) {
+            this.id = post.getId();
+            this.username = post.getUser().getUsername();
+            this.content = post.getContent();
+            this.createdAt = post.getCreatedAt();
+            this.pictures = post.getRunRecord().getPictures();
+            this.loveCount = loveCount;
+            this.commentCount = commentCount;
+            this.isLiked = isLiked;
+        }
+    }
 
     @Data
     public static class DTO {
