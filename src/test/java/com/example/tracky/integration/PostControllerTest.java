dece71435581c47e3ee3608bdefe279c44d07963
roni243 <ksri2457@gmail.com>
2025-07-02 09:37:55 +0900

    save test

--- src/test/java/com/example/tracky/integration/PostControllerTest.java
+++ src/test/java/com/example/tracky/integration/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.tracky.integration;
 
 import com.example.tracky.MyRestDoc;
+import com.example.tracky.community.post.PostRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -35,5 +37,31 @@ class PostControllerTest extends MyRestDoc {
         log.debug("✅응답 바디: " + responseBody);
 
         // then
+    }
+
+    @Test
+    @DisplayName("포스트 쓰기 성공")
+    void save_success() throws Exception {
+
+        // given
+        PostRequest.SaveDTO reqDTO = new PostRequest.SaveDTO();
+        reqDTO.setTitle("제목입니다");
+        reqDTO.setContent("내용입니다");
+        reqDTO.setRunRecordId(1);
+
+        String requestBody = om.writeValueAsString(reqDTO);
+
+        log.debug("✅요청 바디: " + requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/s/api/community/posts")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        log.debug("✅응답 바디: " + responseBody);
     }
 }
