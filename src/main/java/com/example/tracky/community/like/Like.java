bc1447953bd676cfbd43949676be455cb0e0ffa4
roni243 <ksri2457@gmail.com>
2025-07-02 18:05:03 +0900

    [Feat] 게시글 조회 및 글쓰기 (#23)
    * 엔티티 만듦
    
    * 엔티티 만듦
    
    * 엔티티 만듦
    
    * 엔티티 수정
    
    * 엔티티 수정
    
    * 엔티티 수정
    
    * 엔티티 수정
    
    * 엔티티 수정
    
    * 대댓글 필드 추가
    
    * 더미 추가
    
    * 더미 수정
    
    * 더미 추가
    
    * 더미 수정
    
    * 더미ì미 수정
    
    * 더미 수정
    
    * 포스트 조회
    
    * 포스트 조회
    
    * 포스트 수정
    
    * listDTO추가
    
    * dto 수정
    
    * 리스트조회
    
    * 리스트 조회
    
    * ì포스트 수정
    
    * 포스트조회 수정
    
    * 포스트 수정
    
    * 포스트 수정
    
    * 포스트 테스트코드 작성
    
    * 테스트 수정
    
    * 컨트롤러 테스트 추가
    
    * 로직 수정
    
    * 글쓰기 만듦
    
    * 수정
    
    * save test
    
    * save test
    
    * 테스트 수정
    
    * 포스트 수정
    
    * 테스트 수정
    
    * 코드 ì수ì 
    
    * 테스트 수정
    
    * í아ìì글쓰기 수정

---
+++ src/main/java/com/example/tracky/community/like/Like.java
@@ -0,0 +1,45 @@
+package com.example.tracky.community.like;
+
+import com.example.tracky.community.post.Post;
+import com.example.tracky.community.post.comment.Comment;
+import com.example.tracky.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "like_tb")
+@Getter
+@NoArgsConstructor
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    // 좋아요 누른 사용자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Comment comment;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Like(Integer id, Post post, User user, Comment comment) {
+        this.id = id;
+        this.post = post;
+        this.user = user;
+        this.comment = comment;
+    }
+}
---
+++ src/main/java/com/example/tracky/community/post/PostController.java
@@ -0,0 +1,38 @@
+package com.example.tracky.community.post;
+
+import com.example.tracky._core.utils.Resp;
+import com.example.tracky.user.User;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/s/api")
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/community/posts")
+    public ResponseEntity<?> getPosts() {
+
+        Integer userId = 1;
+
+        User user = User.builder().id(userId).build();
+
+        List<PostResponse.ListDTO> respDTOs = postService.getPosts(user);
+        return Resp.ok(respDTOs);
+    }
+
+    @PostMapping("/community/posts")
+    public ResponseEntity<?> save(@Valid @RequestBody PostRequest.SaveDTO reqDTO) {
+        Integer userId = 1;
+        User user = User.builder().id(userId).build();
+
+        PostResponse.DTO respDTO = postService.save(reqDTO, user);
+        return Resp.ok(respDTO);
+    }
+}
