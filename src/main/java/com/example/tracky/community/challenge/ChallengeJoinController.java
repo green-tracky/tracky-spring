b5db15e340bcd4b844a8c81f764c17396866953a
jjack-1 <qazwsx0088@gmail.com>
2025-07-02 23:51:41 +0900

    [Feat] 챌린지 나가기 (#28)
    * 환경설정 중간저장
    
    * 환경설정 중간저장
    
    * 패키지 구조 & 에러처리 & 응답객체 & 프로퍼티 세팅완료
    
    * 어플리케이션 프로퍼티 로컬 파일 주석 스페이스바 1칸 멀어진거 수정
    
    * 수정
    
    * 수정
    
    * dev-properties 파일 공백 수정
    
    * s 붙은 폴더 삭제 안된거 수정
    
    * error 패키지 .keep 파일 삭제
    
    * 프로퍼티 주석 위치 수정
    
    * .
    
    * 칼로리계산 & 페이스계산 유틸함수
    
    * dto 만드는 중
    
    * 중간저장
    
    * 엔티티 시간필드 수정
    
    * 날짜 수정 후 엔티티의 생성자 수정
    
    * 중간저장
    
    * 엔티티 필드값 수정 & 이넘 추가
    
    * 요청 DTO 완성
    
    * 요청 DTO toEntity 완성
    
    * save 테스트 완
    
    * run 관련 엔티티 OneToMany 필드값 빌더 생성자에서 제거
    
    * RunRecord 엔티티 totalDistanceMeters 의 타입 Double -> Integer 변경
    
    * 중간저장
    
    * RunRecord avgPace 변수값 avg_pace 로 되어있던거 원래대로 돌림
    
    * 중간저장
    
    * 사진 데이터 수정. 위도 경도 따로 추가 & 사진의 생성날짜 어노테이션 제거
    
    * 전 커밋 메시지 동일
    
    * 중간저장
    
    * Picture 생성자 수정
    
    * save 에 대한 응답 DTO 만듦
    
    * 통합테스트 만들기전
    
    * 중간저장
    
    * 통합테스트 세팅
    
    * 러닝등록 완료(사진빼고)
    
    * s 붙은 폴더 삭제
    
    * s 붙은 폴더 삭제
    
    * 엔티티 수정 RunRecord & RunSegment. 요청 DTO 수정
    
    * 중간저장
    
    * 응답 DTO 수정
    
    * 수정된 엔티티로 테스트
    
    * 러닝 등록 완 - 사진 제외
    
    * 페이스 계산 함수 수정
    
    * 페이스 계산 수정
    
    * 챌린지 조회
    
    * 이넘 추가
    
    * ãenum 수정
    
    * ìenum 수정
    
    * enum 수정
    
    * ì엔티티 수정
    
    * 엔티티 수정
    
    * ìenum 수정
    
    * enum 수정
    
    * enumì 에러 ì 수정
    
    * 중간저장
    
    * ãDTO 수정
    
    * 주석 수정
    
    * 챌린지 조회 ì테스트 작성
    
    * RunLevel 조회
    
    * 챌린지 조회
    
    * 챌린지 조회
    
    * 챌린지 더미 수정
    
    * 중간저장
    
    * 중간저장
    
    * 중간저장
    
    * 만들어진거 분석중
    
    * 챌린지 로ì직 작성 시작
    
    * 중간저장
    
    * ãããDTO 생성
    
    * 챌린지 목록 일단 완성
    
    * 챌린지 목록 보기 완성
    
    * 챌린지 목록보기
    
    * dto ì설계중
    
    * .
    
    * 챌린지 목록보기 완
    
    * 챌린지 상세보기 중
    
    * 중간저장
    
    * 챌린지 상세보기
    
    * 테스트 검증 추가
    
    * 챌린지 참가 완
    
    * feat/challenge-join-delete
    
    ---------
    
    Co-authored-by: harimmon <bhr0502@gmail.com>
    Co-authored-by: roni243 <ksri2457@gmail.com>

--- src/main/java/com/example/tracky/community/challenge/ChallengeJoinController.java
+++ src/main/java/com/example/tracky/community/challenge/ChallengeJoinController.java
@@ -5,10 +5,8 @@ import com.example.tracky.community.challenge.dto.ChallengeJoinResponse;
 import com.example.tracky.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +24,19 @@ public class ChallengeJoinController {
 
         ChallengeJoinResponse.DTO respDTO = challengeJoinService.save(id, user);
         return Resp.ok(respDTO);
+    }
+
+
+    @DeleteMapping("/community/challenges/{id}/joins")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        // 유저 아이디를 임시로 1 로 함
+        Integer userId = 1;
+
+        // 필터에서 가져올거 미리 가져옴 나중에 세션에서 가져와야함
+        User user = User.builder().id(userId).build();
+
+        challengeJoinService.delete(id, user);
+        return Resp.ok(null);
     }
 
 }
