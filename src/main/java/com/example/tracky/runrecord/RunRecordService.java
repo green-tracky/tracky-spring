1b23638595bf7e3a9c48bdb1b889b46a9dd50760
jjack-1 <qazwsx0088@gmail.com>
2025-07-07 15:54:01 +0900

    [Feat] 챌린지 일간 스케줄 (#34)
    * 환경설정 중간저장
    
    * 환경설정 중간저장
    
    * 패키지 구조 & 에러처리 & 응답객체 & 프로퍼티 세팅완료
    
    * 어플리케이션 프로퍼티 로컬 파일 주석 스페이스바 1칸 멀어진거 수정
    
    * 수정
    
    * 수정
    
    * dev-properties 파일 공백 수정
    
    * s 붙은 폴더 삭제 안된거 수정
    
    * error 패키지 .keep 파일 삭제
    
    * 프로퍼티 주석 위치 수정
    
    * .
    
    * 칼로리계산 & 페이스계산 유틸함수
    
    * dto 만드는 중
    
    * 중간저장
    
    * 엔티티 시간필드 수정
    
    * 날짜 수정 후 엔티티의 생성자 수정
    
    * 중간저장
    
    * 엔티티 필드값 수정 & 이넘 추가
    
    * 요청 DTO 완성
    
    * 요청 DTO toEntity 완성
    
    * save 테스트 완
    
    * run 관련 엔티티 OneToMany 필드값 빌더 생성자에서 제거
    
    * RunRecord 엔티티 totalDistanceMeters 의 타입 Double -> Integer 변경
    
    * 중간저장
    
    * RunRecord avgPace 변수값 avg_pace 로 되어있던거 원래대로 돌림
    
    * 중간저장
    
    * 사진 데이터 수정. 위도 경도 따로 추가 & 사진의 생성날짜 어노테이션 제거
    
    * 전 커밋 메시지 동일
    
    * 중간저장
    
    * Picture 생성자 수정
    
    * save 에 대한 응답 DTO 만듦
    
    * 통합테스트 만들기전
    
    * 중간저장
    
    * 통합테스트 세팅
    
    * 러닝등록 완료(사진빼고)
    
    * s 붙은 폴더 삭제
    
    * s 붙은 폴더 삭제
    
    * 엔티티 수정 RunRecord & RunSegment. 요청 DTO 수정
    
    * 중간저장
    
    * 응답 DTO 수정
    
    * 수정된 엔티티로 테스트
    
    * 러닝 등록 완 - 사진 제외
    
    * 페이스 계산 함수 수정
    
    * 페이스 계산 수정
    
    * .
    
    * 목록에 횟수넣기
    
    * 뱃지목록 표시 완료
    
    * 활동화면에도 횟수 출력 완료
    
    * 러닝시 챌린지 보상 획득
    
    * 야ê약간수정
    
    * 챌린지 엔티티 필드값 추가
    
    * 중간저장
    
    * 챌린지 스케줄 일간 처리 - 종료 및 사설챌린지 보상
    
    ---------
    
    Co-authored-by: harimmon <bhr0502@gmail.com>

--- src/main/java/com/example/tracky/runrecord/RunRecordService.java
+++ src/main/java/com/example/tracky/runrecord/RunRecordService.java
@@ -4,6 +4,7 @@ import com.example.tracky._core.constant.Constant;
 import com.example.tracky._core.error.enums.ErrorCodeEnum;
 import com.example.tracky._core.error.ex.ExceptionApi403;
 import com.example.tracky._core.error.ex.ExceptionApi404;
+import com.example.tracky.community.challenges.ChallengeRewardService;
 import com.example.tracky.community.challenges.domain.UserChallengeReward;
 import com.example.tracky.community.challenges.repository.UserChallengeRewardRepository;
 import com.example.tracky.runrecord.dto.*;
@@ -41,6 +42,7 @@ public class RunRecordService {
     private final UserRepository userRepository;
     private final RunLevelRepository runLevelRepository;
     private final UserChallengeRewardRepository userChallengeRewardRepository;
+    private final ChallengeRewardService challengeRewardService;
 
     /**
      * 러닝 상세 조회
@@ -154,6 +156,7 @@ public class RunRecordService {
         List<RunRecord> recentRunRecords = runRecordsRepository.findTop3ByUserIdOrderByCreatedAtJoinBadgeAchv(user.getId());
         List<RecentRunsDTO> recentRunList = recentRunRecords.stream()
                 .map(r -> new RecentRunsDTO(r))
+                .limit(3)
                 .toList();
 
         // 6. 주차 라벨 생성 (기준 baseDate가 속한 '년-월'에 해당하는 주차만 필터링)
@@ -299,6 +302,7 @@ public class RunRecordService {
         List<RunRecord> recentRunRecords = runRecordsRepository.findTop3ByUserIdOrderByCreatedAtJoinBadgeAchv(user.getId());
         List<RecentRunsDTO> recentRunList = recentRunRecords.stream()
                 .map(r -> new RecentRunsDTO(r))
+                .limit(3)
                 .toList();
 
         // 6. 기록이 있는 월/연도 목록 구성
@@ -427,6 +431,7 @@ public class RunRecordService {
         List<RunRecord> recentRunRecords = runRecordsRepository.findTop3ByUserIdOrderByCreatedAtJoinBadgeAchv(user.getId());
         List<RecentRunsDTO> recentRunList = recentRunRecords.stream()
                 .map(r -> new RecentRunsDTO(r))
+                .limit(3)
                 .toList();
 
         // 6. 주간 평균 통계 계산
@@ -556,6 +561,7 @@ public class RunRecordService {
         List<RunRecord> recentRunRecords = runRecordsRepository.findTop3ByUserIdOrderByCreatedAtJoinBadgeAchv(user.getId());
         List<RecentRunsDTO> recentRunList = recentRunRecords.stream()
                 .map(r -> new RecentRunsDTO(r))
+                .limit(3)
                 .toList();
 
         // 6. 현재 레벨, 총 거리, 다음 레벨까지 거리 계산
@@ -760,7 +766,10 @@ public class RunRecordService {
         // 4. 레벨업 서비스를 호출하여 사용자의 레벨을 업데이트합니다.
         runLevelService.updateUserLevelIfNeeded(user);
 
-        // 5. 최종적으로, 저장된 기록과 새로 획득한 뱃지 목록을 DTO로 감싸 컨트롤러에 반환합니다.
+        // 5. 러닝 저장시 챌린지 보상 획득(공개, 사설(완주자))
+        List<UserChallengeReward> awardedChallengeRewardsPS = challengeRewardService.checkAndAwardChallengeRewards(user);
+
+        // 6. 최종적으로, 저장된 기록과 새로 획득한 뱃지 목록을 DTO로 감싸 컨트롤러에 반환합니다.
         return new RunRecordResponse.SaveDTO(runRecordPS, awardedBadgesPS);
 
     }
