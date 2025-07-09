88eda997b9b4a9e659099b974ceecf8ecb64bd14
JM <lsaoe157@naver.com>
2025-07-09 15:30:05 +0900

    수정 완료

--- src/main/java/com/example/tracky/user/friends/friendinvite/FriendInviteRequest.java
+++ src/main/java/com/example/tracky/user/friends/friendinvite/FriendInviteRequest.java
@@ -1,7 +1,6 @@
 package com.example.tracky.user.friends.friendinvite;
 
-import com.example.tracky.user.User;
-import com.example.tracky.user.friends.friendinvite.enums.InviteStatusType;
+import com.example.tracky._core.enums.InviteStatusEnum;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -11,15 +10,15 @@ public class FriendInviteRequest {
     @Data
     public static class SaveDTO {
         private Integer id;
-        private User fromUser;
-        private User toUser;
-        private InviteStatusType status;
+        private Integer fromUser;
+        private Integer toUser;
+        private InviteStatusEnum status;
         private LocalDateTime createdAt;
 
         public SaveDTO(FriendInvite invite) {
             this.id = invite.getId();
-            this.fromUser = invite.getFromUser();
-            this.toUser = invite.getToUser();
+            this.fromUser = invite.getFromUser().getId();
+            this.toUser = invite.getToUser().getId();
             this.status = invite.getStatus();
             this.createdAt = invite.getCreatedAt();
         }
