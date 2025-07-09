f7e60086d910d43ebad02b888aee71b8e96b9050
roni243 <ksri2457@gmail.com>
2025-07-09 09:27:14 +0900

    수정

--- src/main/java/com/example/tracky/community/posts/comments/CommentController.java
+++ src/main/java/com/example/tracky/community/posts/comments/CommentController.java
@@ -3,15 +3,13 @@ package com.example.tracky.community.posts.comments;
 import com.example.tracky._core.utils.Resp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/s/api")
 public class CommentController {
 
     private final CommentService commentService;
