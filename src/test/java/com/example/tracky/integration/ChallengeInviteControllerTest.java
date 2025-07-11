bca34ba748e5daa13ada7b5818bf8224b3392dcc
PJumMin <129253604+PJumMin@users.noreply.github.com>
2025-07-11 13:47:10 +0900

    [Feat] 챌린지 초대 (#55)
    * 파일 생성
    
    * 저장
    
    * 리더보드 조í
    
    * 랭킹 OK
    
    * 상대가 친구 요청을 하였을 경우 추가
    
    * 필터 완료
    
    * year 수정
    
    * challenge leaderboard OK
    
    * 수정사항 완료
    
    * merge
    
    * 오타 수정
    
    * enum 이름 변경
    
    * 수정
    
    * 저장
    
    * 요청 목록 완료
    
    * 친구 추가 ㅗㄴ료 + 주석 추가
    
    * errorEnum추가
    
    * 친구 추가 완료
    
    * TODO 수정
    
    * 수정사항 완료
    
    * 주석 수정
    
    * 수정 완료
    
    * 수정
    
    * 수정 완료
    
    * 수저아항 완료
    
    * 오ëinvite ¥ 수정
    
    * api 주소 수정
    
    * 챌린지 초대 완료
    
    * 친구 여러명 초대 가능
    
    * 중복 제거
    
    * 수정 완료
    
    * 저장
    
    * 수락 완료
    
    * 통합테스트 만드는 중
    
    * 통합테스트 + 수정사항 완료
    
    * 수정사항 완료
    
    * 저장

---
+++ src/test/java/com/example/tracky/integration/ChallengeInviteControllerTest.java
@@ -0,0 +1,154 @@
+package com.example.tracky.integration;
+
+import com.example.tracky.MyRestDoc;
+import com.example.tracky.community.challenges.dto.ChallengeInviteRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // MOCK -> 가짜 환경을 만들어 필요한 의존관계를 다 메모리에 올려서 테스트
+@Slf4j
+class ChallengeInviteControllerTest extends MyRestDoc {
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    void challenges_invite_test() throws Exception {
+        // given
+        Integer challengeid = 1;
+
+        List<Integer> inviteIds = Arrays.asList(3, 4, 5);
+
+        ChallengeInviteRequest.InviteRequestDTO reqDTO = new ChallengeInviteRequest.InviteRequestDTO();
+        reqDTO.setFriendIds(inviteIds);
+
+        String requestBody = om.writeValueAsString(reqDTO);
+        log.debug("✅요청 바디: " + requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/s/api/community/challenges/{id}/invite", challengeid)
+                        .header("Authorization", "Bearer " + fakeToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        log.debug("✅응답 바디: " + responseBody);
+
+        // then
+        actions.andExpect(status().isOk());
+        actions.andExpect(jsonPath("$.msg").value("성공"));
+
+        actions.andExpect(jsonPath("$.data[0].id").value(3));
+        actions.andExpect(jsonPath("$.data[0].fromUser").value(1));
+        actions.andExpect(jsonPath("$.data[0].toUser").value(3));
+        actions.andExpect(jsonPath("$.data[0].challengeId").value(1));
+        actions.andExpect(jsonPath("$.data[0].status").value("대기"));
+
+        // 디버깅 및 문서화 (필요시 주석 해제)
+        // actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
+    }
+
+    @Test
+    void get_available_friends_test() throws Exception {
+        // given
+        Integer challengeid = 1;
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/s/api/community/challenges/{id}/invite/available-friends", challengeid)
+                        .header("Authorization", "Bearer " + fakeToken)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        log.debug("✅응답 바디: " + responseBody);
+
+        // then
+        actions.andExpect(status().isOk());
+        actions.andExpect(jsonPath("$.msg").value("성공"));
+
+        actions.andExpect(jsonPath("$.data.[0].id").value(6));
+        actions.andExpect(jsonPath("$.data.[0].profileUrl").value("http://example.com/profiles/leo.jpg"));
+        actions.andExpect(jsonPath("$.data.[0].username").value("leo"));
+
+        // 디버깅 및 문서화 (필요시 주석 해제)
+        // actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
+    }
+
+    @Test
+    void friend_inviteAccept_test() throws Exception {
+        // given
+        Integer inviteId = 1;
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/s/api/community/challenges/{id}/invite/accept", inviteId)
+                        .header("Authorization", "Bearer " + fakeToken)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        log.debug("✅응답 바디: " + responseBody);
+
+        // then
+        actions.andExpect(status().isOk());
+        actions.andExpect(jsonPath("$.msg").value("성공"));
+
+        actions.andExpect(jsonPath("$.data.id").value(1));
+        actions.andExpect(jsonPath("$.data.status").value("수락"));
+
+        // 디버깅 및 문서화 (필요시 주석 해제)
+        // actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @Test
+    void friend_inviteReject_test() throws Exception {
+        // given
+        Integer inviteId = 1;
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/s/api/community/challenges/{id}/invite/reject", inviteId)
+                        .header("Authorization", "Bearer " + fakeToken)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        log.debug("✅응답 바디: " + responseBody);
+
+        // then
+        actions.andExpect(status().isOk());
+        actions.andExpect(jsonPath("$.msg").value("성공"));
+
+        actions.andExpect(jsonPath("$.data.id").value(1));
+        actions.andExpect(jsonPath("$.data.status").value("거절"));
+
+        // 디버깅 및 문서화 (필요시 주석 해제)
+        // actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+
+}
