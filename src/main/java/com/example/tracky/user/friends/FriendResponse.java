452c4d6924ba39c79e96e7fc66e71a1b325c59f1
PJumMin <129253604+PJumMin@users.noreply.github.com>
2025-07-11 14:16:15 +0900

    [Feat] 친구 찾기 통합테스트 (#57)
    * 파일 생성
    
    * 저장
    
    * 리더보드 조í
    
    * 랭킹 OK
    
    * 상대가 친구 요청을 하였을 경우 추가
    
    * 필터 완료
    
    * year 수정
    
    * challenge leaderboard OK
    
    * 수정사항 완료
    
    * merge
    
    * 오타 수정
    
    * enum 이름 변경
    
    * 수정
    
    * 저장
    
    * 요청 목록 완료
    
    * 친구 추가 ㅗㄴ료 + 주석 추가
    
    * errorEnum추가
    
    * 친구 추가 완료
    
    * TODO 수정
    
    * 수정사항 완료
    
    * 주석 수정
    
    * 수정 완료
    
    * 수정
    
    * 수정 완료
    
    * 수저아항 완료
    
    * 오ëinvite ¥ 수정
    
    * List를 DTO로 반환 중
    
    * 친구 검색 완료
    
    * 일부 검색 안되는 유저 해결 완료
    
    * merge
    
    * 테스트 폴더 삭제
    
    * 친구 목록 불러오기
    
    * 통합테슽 완료
    
    * 수정 사항 완료

--- src/main/java/com/example/tracky/user/friends/FriendResponse.java
+++ src/main/java/com/example/tracky/user/friends/FriendResponse.java
@@ -3,6 +3,8 @@ package com.example.tracky.user.friends;
 import com.example.tracky.user.User;
 import lombok.Data;
 
+import java.util.List;
+
 public class FriendResponse {
     @Data
     public static class SearchDTO {
@@ -11,6 +13,28 @@ public class FriendResponse {
         private String username;
 
         public SearchDTO(User user) {
+            this.id = user.getId();
+            this.profileUrl = user.getProfileUrl();
+            this.username = user.getUsername();
+        }
+    }
+
+    @Data
+    public static class FriendListDTO {
+        private List<UserDTO> FriendList;
+
+        public FriendListDTO(List<UserDTO> friendList) {
+            FriendList = friendList;
+        }
+    }
+
+    @Data
+    public static class UserDTO {
+        private Integer id;
+        private String profileUrl;
+        private String username;
+
+        public UserDTO(User user) {
             this.id = user.getId();
             this.profileUrl = user.getProfileUrl();
             this.username = user.getUsername();
